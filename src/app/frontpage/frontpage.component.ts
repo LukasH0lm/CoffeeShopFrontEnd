4cb0a44ba452d0a7472de982d13a5f0ce2014c78
sunwar01 <sunwar01@easv365.dk>
2023-11-27 14:47:32 +0100

    Added angular custom buttons.

--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Router} from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-frontpage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.css'
 })
 export class FrontpageComponent {
+  constructor(private router: Router) {}
+  navigateToOrderPage() {
+    this.router.navigate(['/Order']);
+  }
 
 }
