7ab0d77d2a8617d3665a7bc29727c261459d5950
AlexanderF23 <76620504+AlexanderF23@users.noreply.github.com>
2023-11-30 10:49:57 +0100

    Footer first version complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeadercomponentComponent} from "./headercomponent/headercomponent.component";
+import {FootercomponentComponent} from "./footercomponent/footercomponent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, HeadercomponentComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, HeadercomponentComponent, FootercomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
