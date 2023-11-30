d7140981a0ef2b4c46af88e357ec0a506a0283cb
AlexanderF23 <76620504+AlexanderF23@users.noreply.github.com>
2023-11-30 10:50:37 +0100

    Footer first version complete

---
+++ src/app/footercomponent/footercomponent.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footercomponent',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footercomponent.component.html',
+  styleUrl: './footercomponent.component.css'
+})
+export class FootercomponentComponent {
+
+}
